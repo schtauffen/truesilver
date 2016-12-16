b5e9893cb9bdf1643f64db6d7e529f6d6cf33c76
Zach Dahl <z.schtauffen@gmail.com>
2016-12-15 21:15:59 -0600

    fix

--- index.js
+++ index.js
@@ -1,90 +1,87 @@
-module.exports = {
-  selectState: selectState,
-  bindActions: bindActions,
-  component: Component()
-}
-
-function selectState (selector) {
-  return function (context) {
-    return selector(context.getState())
+(function () {
+  var truesilver = {
+    selectState: selectState,
+    bindActions: bindActions,
+    component: Component()
   }
-}
 
-function bindActions (actions) {
-  return function (context) {
-    var i
-    var keys = Object.keys(actions)
-    var il = keys.length
-    var out = {}
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = truesilver
+  } else {
+    window.truesilver = truesilver
+  }
 
-    for (i = 0; i < il; ++i) {
-      out[keys[i]] = function (val) {
-        return function () {
-          context.dispatch(actions[keys[i]](val))
-        }
-      }
+  function selectState (selector) {
+    return function (context) {
+      return selector(context.getState())
+    }
+  }
+
+  function bindActions (actions) {
+    return function (context) {
+      return Object.keys(actions)
+        .reduce(function (out, key) {
+          out[key] = function (val) {
+            return function () {
+              context.dispatch(actions[key](val))
+            }
+          }
+          return out
+        }, {})
+    }
+  }
+
+  function Component (context) {
+    context = typeof context === 'undefined' ? {} : context
+
+    component.replaceContext = function (pContext) {
+      context = pContext
     }
 
-    return out
-  }
-}
-
-function Component (context) {
-  context = typeof context === 'undefined' ? {} : context
-
-  component.replaceContext = function (pContext) {
-    context = pContext
-  }
-
-  Object.defineProperty(component, 'context', {
-    get: function () {
-      return context
-    },
-    set: function (props) {
-      return Object.assign(context, props)
-    }
-  })
-
-  component.withContext = Component
-
-  return component
-
-  function component (initializor, selector) {
-
-    if (Array.isArray(selector) || arguments.length > 2) {
-      var i
-      var il = arguments.length
-      var selectors = []
-
-      for (i = 1; i < il; ++i) {
-        selectors = selectors.concat(arguments[i])
+    Object.defineProperty(component, 'context', {
+      get: function () {
+        return context
+      },
+      set: function (props) {
+        return Object.assign(context, props)
       }
+    })
 
-      selector = function (context) {
+    component.withContext = Component
+
+    return component
+
+    function component (initializor, selector) {
+
+      if (Array.isArray(selector) || arguments.length > 2) {
         var i
-        var il = selectors.length
-        var out = {}
+        var il = arguments.length
+        var selectors = []
 
-        for (i = 0; i < il; ++i) {
-          Object.assign(out, selectors[i](context))
+        for (i = 1; i < il; ++i) {
+          selectors = selectors.concat(arguments[i])
         }
 
-        return out
+        selector = function (context) {
+          return selectors.reduce(function (out, sel) {
+            return Object.assign(out, sel(context))
+          }, {})
+        }
       }
-    }
 
-    return {
-      oninit: function (vnode) {
-        this.view = initializor(this.getContext(vnode))
-      },
-      getContext: function (vnode) {
-          var ctx = selector ? selector(context) : context
-          vnode = vnode ? { vnode: vnode } : {}
-          return Object.assign({}, ctx, vnode)
-      },
-      view: function (vnode) {
-        return this.view(this.getContext(vnode))
+      return {
+        oninit: function (vnode) {
+          this.view = initializor(this.getContext(vnode))
+        },
+        getContext: function (vnode) {
+            var ctx = selector ? selector(context) : context
+            vnode = vnode ? { vnode: vnode } : {}
+            return Object.assign({}, ctx, vnode)
+        },
+        view: function (vnode) {
+          return this.view(this.getContext(vnode))
+        }
       }
     }
   }
-}
+})();
